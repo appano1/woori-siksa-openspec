e7b94c441bcc2ee54a125cbaa9271e438381d483
Sun Lee <36293619+appano1@users.noreply.github.com>
2026-03-17 17:01:28 +0900

    feat(expo): migrate to Expo 55 (#4)
    * feat(expo): migrate to expo 55
    
    * chore(claude): remove expo skills
    
    * fix(expo): remove edgeToEdgeEnabled
    
    edgeToEdgeEnabed is mandatory in sdk 55
    https://expo.dev/changelog/sdk-55

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -12,8 +12,6 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   updates: {
     fallbackToCacheTimeout: 0,
   },
-  newArchEnabled: true,
-  assetBundlePatterns: ["**/*"],
   ios: {
     bundleIdentifier: "your.bundle.identifier",
     supportsTablet: true,
@@ -28,7 +26,6 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       foregroundImage: "./assets/icon-light.png",
       backgroundColor: "#1F104A",
     },
-    edgeToEdgeEnabled: true,
   },
   extra: {
     eas: {
@@ -38,7 +35,6 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   experiments: {
     tsconfigPaths: true,
     typedRoutes: true,
-    reactCanary: true,
     reactCompiler: true,
   },
   plugins: [
